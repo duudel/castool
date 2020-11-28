5c1456e35095e86fb09ff2eb1520cd5f41901f5c
duudel <doodlehp@gmail.com>
2020-11-28 13:51:26 +0200

    Support for durations as strings

--- backend/src/main/scala/castool/cassandra/ColumnValue.scala
+++ backend/src/main/scala/castool/cassandra/ColumnValue.scala
@@ -46,24 +46,6 @@ object ColumnValue {
   implicit val longEncoder: Encoder[Long] = (x: Long) => Json.fromString(x.toString)
 
   implicit val decoder: Decoder[ColumnValue] = deriveDecoder
-    /*List[Decoder[ColumnValue]](
-    Decoder[Null].widen,
-    Decoder[Ascii].widen,
-    Decoder[BigInt].widen,
-    Decoder[Blob].widen,
-    //Decoder[BOOLEAN
-    //Decoder[COUNTER
-    Decoder[Decimal].widen,
-    Decoder[Double].widen,
-    Decoder[Float].widen,
-    Decoder[Integer].widen,
-    Decoder[Timestamp].widen,
-    Decoder[Uuid].widen,
-    Decoder[Varint].widen,
-    Decoder[TimeUuid].widen,
-    Decoder[Text].widen,
-    Decoder[Unknown].widen,
-  ).reduceLeft(_ or _)*/
 
   def fromValue[C <: ColumnValue, T](f: T => C)(v: T): ColumnValue =
     Option(v).map(f).getOrElse(Null)
@@ -79,7 +61,6 @@ object ColumnValue {
         if (buf == null) {
           Null
         } else {
-          println(buf.array().mkString(","))
           val encoder = Base64.getEncoder()
           val b64 = encoder.encode(buf)
           Blob(new String(b64.array()))
@@ -114,6 +95,13 @@ object ColumnValue {
         fromValue(Inet)(row.getInetAddress(index))
       case com.datastax.oss.protocol.internal.ProtocolConstants.DataType.DATE =>
         fromValue(Date)(row.getLocalDate(index))
+      case com.datastax.oss.protocol.internal.ProtocolConstants.DataType.DURATION =>
+        val dur = row.getCqlDuration(index)
+        if (dur == null) {
+          Null
+        } else {
+          fromValue(Duration)(dur.toString())
+        }
 
       case _ =>
         Unknown(columnDef.getName().asCql(true))
@@ -123,65 +111,27 @@ object ColumnValue {
 
 object ColumnValues {
   case object Null extends ColumnValue
-
-  //public static final DataType ASCII = new PrimitiveType(ProtocolConstants.DataType.ASCII);
-  case class Ascii(v: String) extends ColumnValue
-
-  //public static final DataType BIGINT = new PrimitiveType(ProtocolConstants.DataType.BIGINT);
-  case class BigInt(v: Long) extends ColumnValue
-
-  //public static final DataType BLOB = new PrimitiveType(ProtocolConstants.DataType.BLOB);
-  case class Blob(v: String) extends ColumnValue
-
-  //public static final DataType BOOLEAN = new PrimitiveType(ProtocolConstants.DataType.BOOLEAN);
-  case class Bool(v: Boolean) extends ColumnValue
-
-  //public static final DataType COUNTER = new PrimitiveType(ProtocolConstants.DataType.COUNTER);
-  case class Counter(v: Long) extends ColumnValue
-
-  //public static final DataType DECIMAL = new PrimitiveType(ProtocolConstants.DataType.DECIMAL);
-  case class Decimal(v: scala.BigDecimal) extends ColumnValue
-
-  //public static final DataType DOUBLE = new PrimitiveType(ProtocolConstants.DataType.DOUBLE);
-  case class Double(v: Double) extends ColumnValue
-  
-  //public static final DataType FLOAT = new PrimitiveType(ProtocolConstants.DataType.FLOAT);
-  case class Float(v: Float) extends ColumnValue
-
-  //public static final DataType INT = new PrimitiveType(ProtocolConstants.DataType.INT);
-  case class Integer(v: Int) extends ColumnValue
-  //public static final DataType SMALLINT = new PrimitiveType(ProtocolConstants.DataType.SMALLINT);
-  case class SmallInt(v: Short) extends ColumnValue
-  //public static final DataType TINYINT = new PrimitiveType(ProtocolConstants.DataType.TINYINT);
-  case class TinyInt(v: Byte) extends ColumnValue
-  
-  //public static final DataType TIMESTAMP = new PrimitiveType(ProtocolConstants.DataType.TIMESTAMP);
-  case class Timestamp(v: java.time.Instant) extends ColumnValue
-
-  //public static final DataType UUID = new PrimitiveType(ProtocolConstants.DataType.UUID);
-  case class Uuid(v: java.util.UUID) extends ColumnValue
-
-  //public static final DataType VARINT = new PrimitiveType(ProtocolConstants.DataType.VARINT);
-  case class VarInt(v: scala.BigInt) extends ColumnValue
-  
-  //public static final DataType TIMEUUID = new PrimitiveType(ProtocolConstants.DataType.TIMEUUID);
-  case class TimeUuid(v: java.util.UUID) extends ColumnValue
-
-  //public static final DataType TEXT = new PrimitiveType(ProtocolConstants.DataType.VARCHAR);
-  case class Text(v: String) extends ColumnValue
-
-  //public static final DataType INET = new PrimitiveType(ProtocolConstants.DataType.INET);
-  case class Inet(v: InetAddress) extends ColumnValue
-  
-  //public static final DataType DATE = new PrimitiveType(ProtocolConstants.DataType.DATE);
-  case class Date(v: java.time.LocalDate) extends ColumnValue
-  
-  //public static final DataType TIME = new PrimitiveType(ProtocolConstants.DataType.TIME);
-  case class Time(v: java.time.LocalTime) extends ColumnValue
-
   case class Unknown(v: String) extends ColumnValue
-/*
-  public static final DataType DURATION = new PrimitiveType(ProtocolConstants.DataType.DURATION);
-  */
+
+  case class Ascii(v: String) extends ColumnValue // ProtocolConstants.DataType.ASCII
+  case class BigInt(v: Long) extends ColumnValue // ProtocolConstants.DataType.BIGINT
+  case class Blob(v: String) extends ColumnValue // ProtocolConstants.DataType.BLOB
+  case class Bool(v: Boolean) extends ColumnValue // ProtocolConstants.DataType.BOOLEAN
+  case class Counter(v: Long) extends ColumnValue // ProtocolConstants.DataType.COUNTER
+  case class Decimal(v: scala.BigDecimal) extends ColumnValue // ProtocolConstants.DataType.DECIMAL
+  case class Double(v: Double) extends ColumnValue // ProtocolConstants.DataType.DOUBLE
+  case class Float(v: Float) extends ColumnValue // ProtocolConstants.DataType.FLOAT
+  case class Integer(v: Int) extends ColumnValue // ProtocolConstants.DataType.INT
+  case class SmallInt(v: Short) extends ColumnValue // ProtocolConstants.DataType.SMALLINT
+  case class TinyInt(v: Byte) extends ColumnValue // ProtocolConstants.DataType.TINYINT
+  case class Timestamp(v: java.time.Instant) extends ColumnValue // ProtocolConstants.DataType.TIMESTAMP
+  case class Uuid(v: java.util.UUID) extends ColumnValue // ProtocolConstants.DataType.UUID
+  case class VarInt(v: scala.BigInt) extends ColumnValue // ProtocolConstants.DataType.VARINT
+  case class TimeUuid(v: java.util.UUID) extends ColumnValue // ProtocolConstants.DataType.TIMEUUID
+  case class Text(v: String) extends ColumnValue // ProtocolConstants.DataType.VARCHAR
+  case class Inet(v: InetAddress) extends ColumnValue // ProtocolConstants.DataType.INET
+  case class Date(v: java.time.LocalDate) extends ColumnValue // ProtocolConstants.DataType.DATEue
+  case class Time(v: java.time.LocalTime) extends ColumnValue // ProtocolConstants.DataType.TIMEue
+  case class Duration(v: String) extends ColumnValue // ProtocolConstants.DataType.DURATION
 }
 
